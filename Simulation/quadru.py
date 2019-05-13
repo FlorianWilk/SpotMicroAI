145d26376bdf06d5cf2b698bf69165a5b74c4d48
florian.wilk <florian.wilk@gmail.com>
2019-05-13 22:46:55 +0200

    PyBullet screenshot

--- Simulation/quadru.py
+++ Simulation/quadru.py
@@ -3,11 +3,11 @@ cin = p.connect(p.SHARED_MEMORY)
 if (cin < 0):
     cin = p.connect(p.GUI)
 objects = [p.loadURDF("plane.urdf", 0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,1.000000)]
-objects = [p.loadURDF("../urdf/spotmicroai_gen.urdf.xml", 1.192231,-0.869173,0.035383,-0.664986,0.746856,0.000259,-0.000499)]
+objects = [p.loadURDF("../urdf/spotmicroai_gen.urdf.xml", 1.269193,-0.882457,0.137345,-0.071467,0.330220,0.199073,0.919901)]
 objects = p.loadMJCF("../urdf/spotmicroai_gen.urdf.xml")
 ob = objects[0]
-p.resetBasePositionAndOrientation(ob,[1.192231,-0.869173,0.035383],[-0.664986,0.746856,0.000259,-0.000499])
-jointPositions=[ 0.000000, 0.000000, -0.015373, 0.115742, 0.025821, 0.000000, -0.020660, 0.078061, 0.062234, 0.000000, -0.020197, 0.317828, 0.501950, 0.000000, -0.023614, 0.328609, 0.505581, 0.000000 ]
+p.resetBasePositionAndOrientation(ob,[1.269193,-0.882457,0.137345],[-0.071467,0.330220,0.199073,0.919901])
+jointPositions=[ 0.000000, 0.000000, -0.009910, 0.105063, 0.075391, 0.000000, 0.001461, 0.093942, 0.097545, 0.000000, -0.036768, 0.330368, 0.517599, 0.000000, -0.039455, 0.335885, 0.524092, 0.000000 ]
 for jointIndex in range (p.getNumJoints(ob)):
 	p.resetJointState(ob,jointIndex,jointPositions[jointIndex])
 
