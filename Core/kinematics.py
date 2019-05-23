3ba19d0f586a03de0e3a5a2ef4112694c3eb5c9b
florian.wilk <florian.wilk@gmail.com>
2019-05-23 19:36:31 +0200

    Fiest IK PyBullet Tests

--- Core/kinematics.py
+++ Core/kinematics.py
@@ -7,8 +7,6 @@ import matplotlib.animation as animation
 fig = plt.figure()
 data = np.random.rand(2, 25)
 
-
-
 def setupView(limit):
     ax = plt.axes(projection="3d")
     ax.set_xlim(-limit, limit)
@@ -19,101 +17,109 @@ def setupView(limit):
     ax.set_zlabel("Y")
     return ax
 
-setupView(200).view_init(elev=12., azim=28)
+class Kinematic:
 
-omega =  pi/4
-phi =0
-psi = 0
+    def __init__(self):
+        self.l1=50
+        self.l2=20
+        self.l3=115
+        self.l4=130
 
-xm = 0
-ym = 0
-zm = 0
+        self.L = 140
+        self.W = 75
 
-l1=25
-l2=20
-l3=80
-l4=80
+    def bodyIK(self,omega,phi,psi,xm,ym,zm):
+        Rx = np.array([[1,0,0,0],
+                    [0,np.cos(omega),-np.sin(omega),0],
+                    [0,np.sin(omega),np.cos(omega),0],[0,0,0,1]])
+        Ry = np.array([[np.cos(phi),0,np.sin(phi),0],
+                    [0,1,0,0],
+                    [-np.sin(phi),0,np.cos(phi),0],[0,0,0,1]])
+        Rz = np.array([[np.cos(psi),-np.sin(psi),0,0],
+                    [np.sin(psi),np.cos(psi),0,0],[0,0,1,0],[0,0,0,1]])
+        Rxyz=Rx@Ry@Rz
 
-L = 120
-W = 90
+        T = np.array([[0,0,0,xm],[0,0,0,ym],[0,0,0,zm],[0,0,0,0]])
+        Tm = T+Rxyz
 
-Lp=np.array([[100,-100,100,1],[100,-100,-100,1],[-100,-100,100,1],[-100,-100,-100,1]])
+        sHp=np.sin(pi/2)
+        cHp=np.cos(pi/2)
+        (L,W)=(self.L,self.W)
 
-sHp=np.sin(pi/2)
-cHp=np.cos(pi/2)
+        return([Tm @ np.array([[cHp,0,sHp,L/2],[0,1,0,0],[-sHp,0,cHp,W/2],[0,0,0,1]]),
+            Tm @ np.array([[cHp,0,sHp,L/2],[0,1,0,0],[-sHp,0,cHp,-W/2],[0,0,0,1]]),
+            Tm @ np.array([[cHp,0,sHp,-L/2],[0,1,0,0],[-sHp,0,cHp,W/2],[0,0,0,1]]),
+            Tm @ np.array([[cHp,0,sHp,-L/2],[0,1,0,0],[-sHp,0,cHp,-W/2],[0,0,0,1]])
+            ])
 
-Lo=np.array([0,0,0,1])
-
-def bodyIK(omega,phi,psi,xm,ym,zm):
-    Rx = np.array([[1,0,0,0],
-                   [0,np.cos(omega),-np.sin(omega),0],
-                   [0,np.sin(omega),np.cos(omega),0],[0,0,0,1]])
-    Ry = np.array([[np.cos(phi),0,np.sin(phi),0],
-                   [0,1,0,0],
-                   [-np.sin(phi),0,np.cos(phi),0],[0,0,0,1]])
-    Rz = np.array([[np.cos(psi),-np.sin(psi),0,0],
-                   [np.sin(psi),np.cos(psi),0,0],[0,0,1,0],[0,0,0,1]])
-    Rxyz=Rx@Ry@Rz
-
-    T = np.array([[0,0,0,xm],[0,0,0,ym],[0,0,0,zm],[0,0,0,0]])
-    Tm = T+Rxyz
-
-    return([Tm @ np.array([[cHp,0,sHp,L/2],[0,1,0,0],[-sHp,0,cHp,W/2],[0,0,0,1]]),
-           Tm @ np.array([[cHp,0,sHp,L/2],[0,1,0,0],[-sHp,0,cHp,-W/2],[0,0,0,1]]),
-           Tm @ np.array([[cHp,0,sHp,-L/2],[0,1,0,0],[-sHp,0,cHp,W/2],[0,0,0,1]]),
-           Tm @ np.array([[cHp,0,sHp,-L/2],[0,1,0,0],[-sHp,0,cHp,-W/2],[0,0,0,1]])
-           ])
-
-def legIK(point):
-    (x,y,z)=(point[0],point[1],point[2])
-    F=sqrt(x**2+y**2-l1**2)
-    G=F-l2  
-    H=sqrt(G**2+z**2)
-    theta1=-atan2(y,x)-atan2(F,-l1)
-    
-    D=(H**2-l3**2-l4**2)/(2*l3*l4)
-    theta3=acos(D) 
-    
-    theta2=atan2(z,G)-atan2(l4*sin(theta3),l3+l4*cos(theta3))
-    
-    return(theta1,theta2,theta3)
-
-def calcLegPoints(angles):
-    (theta1,theta2,theta3)=angles
-    theta23=theta2+theta3
-
-    T0=Lo
-    T1=T0+np.array([-l1*cos(theta1),l1*sin(theta1),0,0])
-    T2=T1+np.array([-l2*sin(theta1),-l2*cos(theta1),0,0])
-    T3=T2+np.array([-l3*sin(theta1)*cos(theta2),-l3*cos(theta1)*cos(theta2),l3*sin(theta2),0])
-    T4=T3+np.array([-l4*sin(theta1)*cos(theta23),-l4*cos(theta1)*cos(theta23),l4*sin(theta23),0])
+    def legIK(self,point):
+        (x,y,z)=(point[0],point[1],point[2])
+        (l1,l2,l3,l4)=(self.l1,self.l2,self.l3,self.l4)
         
-    return np.array([T0,T1,T2,T3,T4])
+        F=sqrt(x**2+y**2-l1**2)
+        G=F-l2  
+        H=sqrt(G**2+z**2)
+        theta1=-atan2(y,x)-atan2(F,-l1)
+        
+        D=(H**2-l3**2-l4**2)/(2*l3*l4)
+        theta3=acos(D) 
+        
+        theta2=atan2(z,G)-atan2(l4*sin(theta3),l3+l4*cos(theta3))
+        
+        return(theta1,theta2,theta3)
 
-def drawLegPoints(p):
-    plt.plot([x[0] for x in p],[x[2] for x in p],[x[1] for x in p], 'k-', lw=3)
-    plt.plot([p[0][0]],[p[0][2]],[p[0][1]],'bo',lw=2)
-    plt.plot([p[4][0]],[p[4][2]],[p[4][1]],'ro',lw=2)    
+    def calcLegPoints(self,angles):
+        (l1,l2,l3,l4)=(self.l1,self.l2,self.l3,self.l4)
+        (theta1,theta2,theta3)=angles
+        theta23=theta2+theta3
 
-def drawLegPair(Tl,Tr,Ll,Lr):
-    Ix=np.array([[-1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
-    drawLegPoints([Tl@x for x in calcLegPoints(legIK(np.linalg.inv(Tl)@Ll))])
-    drawLegPoints([Tr@Ix@x for x in calcLegPoints(legIK(Ix@np.linalg.inv(Tr)@Lr))])
-    
-def drawRobot(Lp,angles,center):
-    (omega,phi,psi)=angles
-    (xm,ym,zm)=center
-    
-    FP=[0,0,0,1]
-    (Tlf,Trf,Tlb,Trb)= bodyIK(omega,phi,psi,xm,ym,zm)
-    CP=[x@FP for x in [Tlf,Trf,Tlb,Trb]]
+        T0=np.array([0,0,0,1])
+        T1=T0+np.array([-l1*cos(theta1),l1*sin(theta1),0,0])
+        T2=T1+np.array([-l2*sin(theta1),-l2*cos(theta1),0,0])
+        T3=T2+np.array([-l3*sin(theta1)*cos(theta2),-l3*cos(theta1)*cos(theta2),l3*sin(theta2),0])
+        T4=T3+np.array([-l4*sin(theta1)*cos(theta23),-l4*cos(theta1)*cos(theta23),l4*sin(theta23),0])
+            
+        return np.array([T0,T1,T2,T3,T4])
 
-    CPs=[CP[x] for x in [0,1,3,2,0]]
-    plt.plot([x[0] for x in CPs],[x[2] for x in CPs],[x[1] for x in CPs], 'bo-', lw=2)
+    def drawLegPoints(self,p):
+        plt.plot([x[0] for x in p],[x[2] for x in p],[x[1] for x in p], 'k-', lw=3)
+        plt.plot([p[0][0]],[p[0][2]],[p[0][1]],'bo',lw=2)
+        plt.plot([p[4][0]],[p[4][2]],[p[4][1]],'ro',lw=2)    
 
-    drawLegPair(Tlf,Trf,Lp[0],Lp[1])
-    drawLegPair(Tlb,Trb,Lp[2],Lp[3])
+    def drawLegPair(self,Tl,Tr,Ll,Lr):
+        Ix=np.array([[-1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+        self.drawLegPoints([Tl@x for x in self.calcLegPoints(self.legIK(np.linalg.inv(Tl)@Ll))])
+        self.drawLegPoints([Tr@Ix@x for x in self.calcLegPoints(self.legIK(Ix@np.linalg.inv(Tr)@Lr))])
+        
+    def drawRobot(self,Lp,angles,center):
+        (omega,phi,psi)=angles
+        (xm,ym,zm)=center
+        
+        FP=[0,0,0,1]
+        (Tlf,Trf,Tlb,Trb)= self.bodyIK(omega,phi,psi,xm,ym,zm)
+        CP=[x@FP for x in [Tlf,Trf,Tlb,Trb]]
+
+        CPs=[CP[x] for x in [0,1,3,2,0]]
+        plt.plot([x[0] for x in CPs],[x[2] for x in CPs],[x[1] for x in CPs], 'bo-', lw=2)
+
+        self.drawLegPair(Tlf,Trf,Lp[0],Lp[1])
+        self.drawLegPair(Tlb,Trb,Lp[2],Lp[3])
+
+    def calcIK(self,Lp,angles,center):
+        (omega,phi,psi)=angles
+        (xm,ym,zm)=center
+        
+        (Tlf,Trf,Tlb,Trb)= self.bodyIK(omega,phi,psi,xm,ym,zm)
+
+        Ix=np.array([[-1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+        return np.array([self.legIK(np.linalg.inv(Tlf)@Lp[0]),
+        self.legIK(Ix@np.linalg.inv(Trf)@Lp[1]),
+        self.legIK(np.linalg.inv(Tlb)@Lp[2]),
+        self.legIK(Ix@np.linalg.inv(Trb)@Lp[3])])
+
 
 if __name__=="__main__":
-    drawRobot(Lp,(0,0,0),(0,0,0))
+    setupView(200).view_init(elev=12., azim=28)
+    Lp=np.array([[100,-100,100,1],[100,-100,-100,1],[-100,-100,100,1],[-100,-100,-100,1]])
+    Kinematic().drawRobot(Lp,(0,0,0),(0,0,0))
     plt.show()
--- Core/test.py
+++ Core/test.py
@@ -1,22 +1,99 @@
+from kinematics import Kinematic
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import pybullet as p
+import pybullet_data
+import time
+import math
 
+useMaximalCoordinates = False
+useRealTime = 0
+fixedTimeStep = 1. / 100
+numSolverIterations = 50
+t = 0.0
+t_end = t + 100
+physId = p.connect(p.SHARED_MEMORY)
+if (physId < 0):
+  p.connect(p.GUI)
+angle = 0   
+orn = p.getQuaternionFromEuler([0, angle, 0])
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.loadURDF("plane.urdf", [0, 0, 0], orn)
 
-def update_line(num, data, line):
-    line.set_data(data[..., :num])
-    return line,
-fig1 = plt.figure()
+p.setTimeOut(4000000)
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
+p.setGravity(0, 0, -9.81)
+p.setTimeStep(fixedTimeStep)
 
-data = np.random.rand(2, 25)
-l, = plt.plot([], [], 'r-')
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-plt.xlabel('x')
-plt.title('test')
-line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l),
-                                   interval=50, blit=True)
-plt.show()
+orn = p.getQuaternionFromEuler([0, 0, 0.0])
+p.setRealTimeSimulation(useRealTime)
+quadruped = p.loadURDF("../urdf/spotmicroai_gen.urdf.xml", [1, -1, .3],
+                       orn,
+                       useFixedBase=False,
+                       useMaximalCoordinates=useMaximalCoordinates,
+                       flags=p.URDF_USE_IMPLICIT_CYLINDER)
+nJoints = p.getNumJoints(quadruped)
+jointNameToId = {}
+for i in range(nJoints):
+  jointInfo = p.getJointInfo(quadruped, i)
+  jointNameToId[jointInfo[1].decode('UTF-8')] = jointInfo[0]
+
+front_left_shoulder = jointNameToId['front_left_shoulder']
+front_left_leg = jointNameToId['front_left_leg']
+front_left_foot = jointNameToId['front_left_foot']
+front_left_toe = jointNameToId['front_left_toe']
+
+front_right_shoulder = jointNameToId['front_right_shoulder']
+front_right_leg = jointNameToId['front_right_leg']
+front_right_foot = jointNameToId['front_right_foot']
+front_right_toe = jointNameToId['front_right_toe']
+
+rear_left_shoulder = jointNameToId['rear_left_shoulder']
+rear_left_leg = jointNameToId['rear_left_leg']
+rear_left_foot = jointNameToId['rear_left_foot']
+rear_left_toe = jointNameToId['rear_left_toe']
+
+rear_right_shoulder = jointNameToId['rear_right_shoulder']
+rear_right_leg = jointNameToId['rear_right_leg']
+rear_right_foot = jointNameToId['rear_right_foot']
+rear_right_toe = jointNameToId['rear_right_toe']
+Lp=np.array([[100,-100,100,1],[100,-100,-100,1],[-100,-100,100,1],[-100,-100,-100,1]])
+kin=Kinematic()
+p.setRealTimeSimulation(useRealTime)
+kp = 1
+kd = .5
+maxForce = 3.5
+ref_time = time.time()
+for i in range (nJoints):
+	p.changeDynamics(quadruped,i,localInertiaDiagonal=[0.000001,0.000001,0.000001])
+
+while (1):
+
+    if (useRealTime):
+        t = time.time() - ref_time
+    else:
+        t = t + fixedTimeStep
+
+    #Lp[0][0]+=0.2
+    pia=1*math.sin((t/10))
+    angles=kin.calcIK(Lp,(0,0,0),(0,0,0))
+    #joints=[jointNameToId[x+"_"+a] for x in ['front_left','front_right','rear_left','rear_right']  for a in ['shoulder','leg','foot']  ]
+    idx=0
+    for lx,leg in enumerate(['front_left','front_right','rear_left','rear_right']):
+        for px,part in enumerate(['shoulder','leg','foot']):
+            j=jointNameToId[leg+"_"+part]
+            aa=(angles[lx][px])
+            if(px==0 and lx==0):
+                print(aa)
+            p.setJointMotorControl2(bodyIndex=quadruped,
+                                    jointIndex=j,
+                                    controlMode=p.POSITION_CONTROL,
+                                    targetPosition= math.pi,
+                                    positionGain=kp,
+                                    velocityGain=kd,
+                                    force=maxForce)
+
+            idx+=1
+
+    if (useRealTime == 0):
+        p.stepSimulation()
+        time.sleep(fixedTimeStep)
