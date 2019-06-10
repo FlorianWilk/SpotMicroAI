b439ad808a7bc24436e37f9c14a66578a3dcf768
Florian <florian@ri-digital0452.fritz.box>
2019-06-10 18:33:48 +0200

    added OpenAI Gym Skeleton

---
+++ Core/gym_spotmicroai/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_spotmicroai.envs.spotmicroai_env import SpotMicroAIEnv
---
+++ Core/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+ 
+setup(name='gym_spotmicroai',
+      version='0.1',
+      install_requires=['gym', 'numpy', 'pybullet']
+)
